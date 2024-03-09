c2d03dbc40469a0c23bad7093b761cb6a957ba41
Ray <buttrafay980@gmail.com>
2024-03-09 18:37:54 +0500

    updated

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const https = require( "http" );
 const express = require( "express" );
+const cors = require( "cors" );
 const app = express();
 const server = https.createServer( app );
 const SocketIO = require( "socket.io" );
-const io = new SocketIO.Server( server );
+const io = new SocketIO.Server( server, {
+  cors: {
+    origin: "http://localhost:3002"
+  }
+} );
 const PORT = 5261;
 
-app.get( "/", ( req, res ) => {
-  res.status( 200 ).send( "Hello, world!" );
+
+app.use( cors() );
+
+
+app.get( "/", ( _, res ) => {
+  res.send( "Hello, world!" );
 } );
 
 io.on( "connection", ( socket ) => {
   console.log( socket.id, " connected!" );
+  socket.emit( "connection", { msg: "connected" } );
+
+  socket.on( "newConnection", ( data ) => {
+    console.log( "data: ", data );
+    socket.id = data.id;
+  } );
+
+  socket.on( "disconnect", () => {
+    console.log( socket.id, " disconnected!" );
+  } );
 } );
 
 
 
 
-server.listen( PORT, () => console.log( `running on port ${ PORT }` ) );
+io.listen( PORT );
+
+// server.listen( PORT, () => console.log( `running on port ${ PORT }` ) );
